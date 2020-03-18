d86514481adbd9e8155018ef2a43c9bf47f1b653
g4br-4d3v <gabrielalbuquerque-ads@hotmail.com>
2020-03-18 17:30:40 -0300

    Migrations

---
+++ 005/database/migrations/2020_03_18_200602_create_categories_table.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * 
+ * Created with:
+ *      php artisan make:migration create_categories_table
+ * 
+ * Can Undo with:
+ *      php artisan migrater:rollback (--step=<nº of edits> OPTIONAL)
+ * 
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
