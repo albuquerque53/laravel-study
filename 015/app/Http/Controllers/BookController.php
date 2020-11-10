ce2cbdc96d62f55829e04d20e0a87d57e95ae4be
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-11-09 22:02:20 -0300

    :sparkles: Controller methods

---
+++ 015/app/Http/Controllers/BookController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = Book::all();
+
+        return json_encode($books);
+    }
+}
