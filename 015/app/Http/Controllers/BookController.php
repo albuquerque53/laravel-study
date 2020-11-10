eb61b671170a97917ba683c3d889bb99bafd7b05
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-11-10 19:06:18 -0300

    :fire: Removed unnecessary methods

--- 015/app/Http/Controllers/BookController.php
+++ 015/app/Http/Controllers/BookController.php
@@ -3,20 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class BookController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
         $books = Book::all();
 
+        if ($books->isEmpty()) {
+            return json_encode([
+                'message' => 'Sorry, an error ocurred :/'
+            ]);
+        }
+
         return json_encode($books);
+    }
+
+    /**
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Book $book)
+    {
+        return json_encode($book);
+    }
+
+    private function checkBooks(Collection $book)
+    {
+        if ($book->empty()) {
+            return false;
+        }
     }
 }
