c24dbe057f6b87b437d036091af9a9f8d129feda
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-11-09 21:20:13 -0300

    :sparkles: Database configured

---
+++ 015/database/factories/BookFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BookFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Book::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->name,
+            'pages' => $this->faker->numberBetween(200, 1200),
+            'author' => $this->faker->name
+        ];
+    }
+}
